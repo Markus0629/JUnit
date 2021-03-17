90eb45422f4d9e820ed3a227869935e025a8c792
Markus0629 <65181555+Markus0629@users.noreply.github.com>
2021-03-17 18:30:39 +0100

    ElMeuNombre

---
+++ ElMeuNombre/src/com/test/ProgramaExecutarElMeuNombre.java
@@ -0,0 +1,24 @@
+package com.test;
+
+public class ProgramaExecutarElMeuNombre {
+
+	public static void main(String[] args) {
+		ElMeuNombre nombre1 = new ElMeuNombre (5);
+		ElMeuNombre nombre2 = new ElMeuNombre (8);
+		
+		
+		System.out.println("El nombre1 = " + nombre1.getNombre());
+		
+		System.out.println("El nombre2 = " + nombre2.getNombre());
+		
+		System.out.println("El nombre1 + nombre2 = " + nombre1.suma(nombre2).getNombre());
+		
+		System.out.println("El nombre1 - nombre2 = " + nombre1.resta(nombre2).getNombre());
+		
+		System.out.println("El nombre1 * nombre2 = " + nombre1.multi(nombre2).getNombre());
+		
+		System.out.println("El nombre1 / nombre2 = " + nombre1.div(nombre2).getNombre());
+		
+	}
+
+}
